8d81787fa538326bd618991f94bdd8477b5826f2
CoderPulkit <75306428+CoderPulkit@users.noreply.github.com>
2021-08-11 16:50:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
+function preload(){
+ song=loadSound("music.mp3");
+}
+
+function setup(){
+ canvas=createCanvas(600,470);
+ canvas.center();
+ video=createCapture(VIDEO);
+ video.hide();
+ posenet=ml5.poseNet(video,modalLoaded);
+ posenet.on('pose',gotPoses);
+}
+
+function draw(){
+ image(video,0,0,600,470);
+}
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1.5);
+}
+
+function modalLoaded(){
+    console.log("Modal Laoded");
+}
+
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+    console.log("Left Wrist X= "+leftWristX+"Left Wrist Y= "+leftWristY);
+    console.log("Right Wrist X= "+rightWristX+"Right Wrist Y= "+rightWristY);
+    
+}
+}
